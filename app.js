ceaca30f0afaf87727bfb430cf99d335f890ce6c
PTHy <30455428+PTHy@users.noreply.github.com>
2018-06-12 18:33:50 +0900

    [ADD]Server running
    running

---
+++ app.js
@@ -0,0 +1,9 @@
+const bodyParser = require('body-parser')
+const express = require('express')
+require('dotenv').config()
+const port = process.env.SERVER_PORT
+const app = express()
+
+const server = app.listen(port, () => {
+  console.log(`Express Server is Running on ${port}`);
+})
